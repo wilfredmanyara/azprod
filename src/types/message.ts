ca2c869bc0e6b0cee067ea5d8e9f6c65bb3f027c
Wilfred Manyara <manyarawilly46@gmail.com>
2023-11-07 11:09:05 +0300

    initial commit

---
+++ src/types/message.ts
@@ -0,0 +1,16 @@
+import Messages from "@/components/chat/Messages";
+import { AppRouter } from "@/trpc";
+import { inferRouterOutputs } from "@trpc/server";
+import { number } from "zod";
+
+type RouterOutput = inferRouterOutputs<AppRouter>
+
+type Messages = RouterOutput["getFileMessages"]["messages"]
+
+type OmitText = Omit<Messages[number], "text">
+
+type ExtendedText = {
+    text: string | JSX.Element
+}
+
+export type ExtendedMessage = OmitText & ExtendedText   
