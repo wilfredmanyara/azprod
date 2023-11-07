5a2cdf8a00c1c9954d00234c54c98923610b06e4
Wilfred Manyara <manyarawilly46@gmail.com>
2023-11-07 13:41:07 +0300

    updated stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
-import {LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
+import {LoginLink, RegisterLink, getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from "lucide-react"
+import UserAccountNav from "./UserAccountNav"
 
 const Navbar = () => {
+
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -16,7 +21,7 @@ const Navbar = () => {
 
 
                     <div className="hidden items-center space-x-4 sm:flex">
-                        <>
+                        {!user ? <>
                         <Link 
                         href='/pricing' 
                         className={buttonVariants({
@@ -38,7 +43,23 @@ const Navbar = () => {
                         })}>
                             Get started <ArrowRight className="ml-1.5 h-5 w-5"/>
                         </RegisterLink>
-                        </>
+                        </> : 
+                        <>
+                        <Link 
+                        href='/dashboard' 
+                        className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm"
+                        })}>
+                            Dashboard
+                        </Link>
+                        <UserAccountNav name={
+                            !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
+                        }
+                        email={user.email ?? ''}
+                        imageUrl={user.picture ?? ''} 
+                        />
+                        </>}
                     </div>
                 </div>
             </MaxWidthWrapper>
---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image"
+import { Avatar, AvatarFallback } from "./ui/avatar"
+import { Button } from "./ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Icons } from "./Icons"
+import Link from "next/link"
+import { CheckCircle, CheckCircle2, Gem } from "lucide-react"
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server"
+
+interface UserAccountNavProps {
+    email: string | undefined
+    name: string
+    imageUrl: string
+}
+
+const UserAccountNav = ({email, imageUrl, name}: UserAccountNavProps) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild className="overflow-visible">
+                <Button className="rounded-full h-8 w-8 aspect-square bg-slate-400">
+                    <Avatar className="relative w-8 h-8">
+                        {imageUrl ? (
+                            <div className="relative aspect-square h-full w-full">
+                                <Image fill src={imageUrl} alt="profile picture" referrerPolicy="no-referrer" />
+                            </div>
+                        ) : <AvatarFallback>
+                            <span className="sr-only ">{name}</span>
+                            <Icons.user className="h-4 w-4 text-zinc-900" />
+                            </AvatarFallback>}
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger> 
+
+            <DropdownMenuContent className="bg-white" align="end" >
+                <div className="flex items-center justify-start gap-2 p-2">
+                    <div className="flex flex-col space-y-0.5 leading-none">
+                        {name && <p className="font-medium text-sm text-black">{name}{" "}<CheckCircle className="text-blue-600 h-3 w-3 items-center inline-block" /></p>}
+                        {email && (
+                            <p className="w-[200px] truncate text-xs text-zinc-700">
+                                {email}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+                <Link href='/dashboard'>Dashboard</Link>
+            </DropdownMenuItem>
+                        
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className="cursor-pointer">
+                <LogoutLink>Log out</LogoutLink>
+            </DropdownMenuItem>
+            </DropdownMenuContent>
+            
+        </DropdownMenu>
+    )
+}
+
+export default UserAccountNav
